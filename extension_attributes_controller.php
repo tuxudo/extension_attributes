6d521e8d78a5292eb05560bd1ed03f26080ce383
tuxudo <tuxudo@users.noreply.github.com>
2023-03-29 13:40:38 -0400

    Update extension_attributes_controller.php

--- extension_attributes_controller.php
+++ extension_attributes_controller.php
@@ -33,6 +33,9 @@ class Extension_attributes_controller extends Module_controller
     **/
     public function get_tab_data($serial_number = '')
     {
+        // Remove non-serial number characters
+        $serial_number = preg_replace("/[^A-Za-z0-9_\-]]/", '', $serial_number);
+
         $obj = new View();
         
         $sql = "SELECT displayname, result
